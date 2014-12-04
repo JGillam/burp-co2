bfb13f5aa233f20a0f4f93dfff5d755250bdea0f
Jason Gillam <Jason.Gillam@gmail.com>
2014-12-03 23:06:51 -0500

    Updated version

--- modules/co2-suite/src/com/professionallyevil/co2/Co2SuiteExtender.java
+++ modules/co2-suite/src/com/professionallyevil/co2/Co2SuiteExtender.java
@@ -35,7 +35,7 @@ import java.awt.*;
  * burp.BurpExtender
  */
 public class Co2SuiteExtender implements IBurpExtender, IExtensionStateListener, Co2Extender {
-    public static final String VERSION = "1.1.2 b";
+    public static final String VERSION = "1.1.3b";
     private Co2ConfigTab configTab;
     private IBurpExtenderCallbacks callbacks;
     private java.util.Timer co2Timer = new java.util.Timer("Co2", false);
