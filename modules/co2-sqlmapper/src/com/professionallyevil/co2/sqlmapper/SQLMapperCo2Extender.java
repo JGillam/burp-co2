b78efc45de751f5c3beef2ceec8be12f3820ab01
Jason Gillam <jason.gillam@gmail.com>
2015-08-10 21:01:42 -0400

    Updated version and release notes for 1.1.8 release.

--- modules/co2-sqlmapper/src/com/professionallyevil/co2/sqlmapper/SQLMapperCo2Extender.java
+++ modules/co2-sqlmapper/src/com/professionallyevil/co2/sqlmapper/SQLMapperCo2Extender.java
@@ -29,7 +29,7 @@ import java.awt.*;
  * burp.BurpExtender
  */
 public class SQLMapperCo2Extender implements IExtensionStateListener, Co2Extender {
-    public static final String VERSION = "1.2.2b";
+    public static final String VERSION = "1.3";
     private IBurpExtenderCallbacks callbacks;
 
     public SQLMapperCo2Extender() {
