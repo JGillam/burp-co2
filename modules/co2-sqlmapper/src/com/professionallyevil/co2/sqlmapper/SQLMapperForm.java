JGillam/burp-co2

commit de8074943afcab88e73165cb977e457d36e84477

Fixed cookie spacing issue.  This fixes issue #10.

modules/co2-sqlmapper/src/com/professionallyevil/co2/sqlmapper/SQLMapperForm.java
@@ -355,10 +355,10 @@ public class SQLMapperForm implements ClipboardOwner, ActionListener, DocumentLi
                 body.append(param.getValue());
                 body.append('&');
             } else if (param.getType() == IParameter.PARAM_COOKIE) {
-                cookies.append(param.getName());
+                cookies.append(param.getName().trim());
                 cookies.append('=');
-                cookies.append(param.getValue());
-                cookies.append("; ");
+                cookies.append(param.getValue().trim());
+                cookies.append(";");
             }
         }
         if (body.length() > 0) {

modules/co2-sqlmapper/src/com/professionallyevil/co2/sqlmapper/SQLMapperForm.form
@@ -64,7 +64,9 @@
                 <preferred-size width="150" height="-1"/>
               </grid>
             </constraints>
-            <properties/>
+            <properties>
+              <toolTipText value="Remove spaces between cookies (due to a bug in SQLMap)"/>
+            </properties>
           </component>
           <component id="3fd17" class="javax.swing.JCheckBox" binding="chkIncludeData">
             <constraints>
@@ -1114,7 +1116,9 @@
                         <preferred-size width="150" height="-1"/>
                       </grid>
                     </constraints>
-                    <properties/>
+                    <properties>
+                      <toolTipText value="Specify protocol &amp; port (e.g. http://127.0.0.1:8081)"/>
+                    </properties>
                   </component>
                   <component id="446a8" class="javax.swing.JLabel">
                     <constraints>

commit 6c53b70be82228f4d8378d48f4c9198e158c0ca9

Removes semicolon, not last character of cookie
Fixes bug introduced in commit de8074943afcab88e73165cb977e457d36e84477.
When spaces were removed, the code truncated the last character of the cookie rather than removing the trailing semicolon

modules/co2-sqlmapper/src/com/professionallyevil/co2/sqlmapper/SQLMapperForm.java
@@ -397,7 +397,7 @@ public class SQLMapperForm implements ClipboardOwner, ActionListener, DocumentLi
             body.deleteCharAt(body.length() - 1);
         }
         if (cookies.length() > 1) {
-            cookies.deleteCharAt(cookies.length() - 2);
+            cookies.deleteCharAt(cookies.length() - 1); //Delete trailing semicolon
         }
         dataTxt.setText(body.toString());
         chkIncludeData.setSelected(dataTxt.getText().length() > 0);